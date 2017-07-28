92a364b4cb21bd5b27be6e5b256b1fffabb5ba5a
Doug Crawford <doug.crawford@intelight-its.com>
2017-07-28 14:21:37 -0700

    fix pause in update-external

--- update-external.bat
+++ update-external.bat
@@ -4,4 +4,4 @@ aws s3 sync s3://intelight-jenkins-artifacts/maxadmin/master/ external/
 aws s3 sync s3://intelight-jenkins-artifacts/max-profile-server/master/ external/
 aws s3 sync s3://intelight-jenkins-artifacts/max-time-server/maxconnect-master/ external/
 aws s3 sync s3://intelight-jenkins-artifacts/max-time-client/max-connect-master/ external/
-pause
+IF %0 == "%~0"  pause
